a484cd63c8c926792b4a229d61a69b7ae8558131
lucasdav <lucassouzadavanso@gmail.com>
2024-02-18 11:49:16 -0300

    criacao logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>Davs Restaurante</div>
+    <Cardapio/>
   </React.StrictMode>
 );
