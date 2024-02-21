f8e95e3ee8db8179ce535c2baa5138d4f157c4cf
lucasdav <lucassouzadavanso@gmail.com>
2024-02-20 23:08:24 -0300

    inclusao buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+
     return (
         <main>
             <nav className={styles.menu}>
@@ -13,7 +17,10 @@ export default function Cardapio() {
                     A casa da massa italiana
                 </div>
             </header>
-        
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+            </section>
         </main>
     )
 }
