7eb044b489ef3107abd59f64126bf3cb7c6dd37a
lucasdav <lucassouzadavanso@gmail.com>
2024-02-21 22:54:53 -0300

    estilizacao filtro

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -3,16 +3,25 @@ import styles from './Filtros.module.scss';
 
 type IOpcao = typeof filtros[0];
 
-export default function Filtros() {
+interface Props {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filtros({ filtro, setFiltro }: Props) {
 
     function selecionarFiltro(opcao: IOpcao) {
-
+        return setFiltro(opcao.id);
     }
 
     return (
     <div className={styles.filtros}>
         {filtros.map((opcao) => (
-            <button className={styles.filtros__filtro} key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+            <button 
+                className={ `
+                ${styles.filtros__filtro} ${filtro === opcao.id ? styles["filtros__filtro--ativo"] : ""}`}
+                key={opcao.id} 
+                onClick={() => selecionarFiltro(opcao)}>
                 {opcao.label}
             </button>
         ))}
