cb114049cd7c4901033119d8afdd029db36366ca
lucasdav <lucassouzadavanso@gmail.com>
2024-02-26 22:26:17 -0300

    reaproveitando menu

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import Buscador from './Buscador';
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
+import Menu from 'components/Menu';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState('');
@@ -13,10 +13,7 @@ export default function Cardapio() {
 
     return (
         <main>
-            <nav className={styles.menu}>
-                <Logo width={100} height={100} />
-                Davs Restaurante
-            </nav>
+            <Menu />
             <header className={styles.header}>
                 <div className={styles.header__text}>
                     A casa da massa italiana
