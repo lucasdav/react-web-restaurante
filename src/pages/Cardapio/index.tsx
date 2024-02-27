98b7642f01351496dfeb8da790b148a4e03c8512
lucasdav <lucassouzadavanso@gmail.com>
2024-02-27 19:11:33 -0300

    reaproveitando o Header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,7 +4,6 @@ import styles from './Cardapio.module.scss';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
-import Menu from 'components/Menu';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState('');
@@ -12,21 +11,14 @@ export default function Cardapio() {
     const [ordenador, setOrdenador] = useState('');
 
     return (
-        <main>
-            <header className={styles.header}>
-                <div className={styles.header__text}>
-                    A casa da massa italiana
-                </div>
-            </header>
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                <Buscador busca={busca} setBusca={setBusca} />
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro={setFiltro} />
-                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-                </div>
-                <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-            </section>
-        </main>
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <Buscador busca={busca} setBusca={setBusca} />
+            <div className={styles.cardapio__filtros}>
+                <Filtros filtro={filtro} setFiltro={setFiltro} />
+                <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+            </div>
+            <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+        </section>
     );
 }
