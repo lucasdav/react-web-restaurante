fae677073de510f2012b3a15e17c0298aaf8c00e
lucasdav <lucassouzadavanso@gmail.com>
2024-02-29 17:32:15 -0300

    usando o useParams

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -2,6 +2,7 @@ import cardapio from 'data/cardapio.json';
 import Item from './Item';
 import styles from './Itens.module.scss';
 import { useEffect, useState } from 'react';
+import { Cardapio } from 'types/Prato';
 
 interface Props {
     busca: string,
@@ -14,7 +15,7 @@ export default function Itens(props: Props) {
     const [lista, setLista] = useState(cardapio);
     const { busca, filtro, ordenador } = props;
 
-    function ordenar(novaLista: typeof cardapio) {
+    function ordenar(novaLista: Cardapio) {
         switch (ordenador) {
             case 'porcao':
                 return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
